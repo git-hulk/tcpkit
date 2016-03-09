a6688043efe6112cd4ed960121d31499244b3288
git-hulk <hulk.website@gmail.com>
2016-03-09 17:41:53 +0800

    MOD: edit example

--- scripts/example.lua
+++ scripts/example.lua
@@ -1,11 +1,3 @@
-local cjson = require "cjson"
-local example_config = {
-    server = "192.168.41.219",
-    port = 3306,
-    device = "en5" 
-}
-
-set_config(example_config)
 function process_packet(item)
     -- item.tv_sec 
     -- time.tv_usec
@@ -14,5 +6,5 @@ function process_packet(item)
     -- item.dst
     -- item.dport
     -- item.payload
-    print("---- packet ----")
+    print(item.payload)
 end
