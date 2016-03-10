5ae0ee09d06fe6892ffc4a2c5cd0803983be4376
git-hulk <hulk.website@gmail.com>
2016-03-10 10:02:43 +0800

    MOD: refactor redis.lua

--- scripts/redis.lua
+++ scripts/redis.lua
@@ -4,10 +4,7 @@ local packet_hash_sec = {}
 local packet_hash_usec = {}
 local packet_hash_cmd = {}
 
-function parse_data(str)
-    if str == nil then
-        return ""
-    end
+function parse_request(str)
     if string.byte(str, 1) ~= 42 then
         return string.sub(str, 1, string.len(str) - 1)
     end
@@ -23,63 +20,60 @@ function parse_data(str)
     return result
 end
 
+function calculate_request_cost(key, item)
+    if packet_hash_cmd[key] then
+        local cost = (item.tv_sec - packet_hash_sec[key]) * 1000
+        cost = cost +  (item.tv_usec - packet_hash_usec[key]) / 1000
+        if string.len(packet_hash_cmd[key]) > 20 then
+            print(string.format("%36s | %1.20s... | %3.3fms", key, packet_hash_cmd[key], cost))
+        else
+            print(string.format("%36s | %-23s | %3.3fms", key, packet_hash_cmd[key], cost))
+        end
+        packet_hash_sec[key] = nil 
+        packet_hash_usec[key] = nil 
+        packet_hash_cmd[key] = nil 
+    end
+end
+
+function store_request(key, item)
+    packet_hash_sec[key] = item.tv_sec
+    packet_hash_usec[key] = item.tv_usec
+    packet_hash_cmd[key] = parse_request(item.payload)
+end
+
 function handle_incoming(item)
     if item.is_client == 1 then
         key = item.dst.. ":" .. item.dport .. " | " .. item.src.. ":" .. item.sport
-        if packet_hash_cmd[key] then
-            local cost = (item.tv_sec - packet_hash_sec[key]) * 1000
-            cost = cost +  (item.tv_usec - packet_hash_usec[key]) / 1000
-            if string.len(packet_hash_cmd[key]) > 20 then
-                print(string.format("%36s | %1.20s... | %3.3fms", key, packet_hash_cmd[key], cost))
-            else
-                print(string.format("%36s | %-23s | %3.3fms", key, packet_hash_cmd[key], cost))
-            end
-            packet_hash_sec[key] = nil 
-            packet_hash_usec[key] = nil 
-            packet_hash_cmd[key] = nil 
-        end
+        calculate_request_cost(key, item)
     else
         key = item.src.. ":" .. item.sport .. " | " .. item.dst.. ":" .. item.dport
-        packet_hash_sec[key] = item.tv_sec
-        packet_hash_usec[key] = item.tv_usec
-        packet_hash_cmd[key] = parse_data(item.payload)
+        store_request(key, item)
     end
 end
 
 function handle_outgoing(item)
     if item.is_client == 1 then
         key = item.src.. ":" .. item.sport .. " | " .. item.dst.. ":" .. item.dport
-        packet_hash_sec[key] = item.tv_sec
-        packet_hash_usec[key] = item.tv_usec
-        packet_hash_cmd[key] = parse_data(item.payload)
+        store_request(key, item)
     else
         key = item.dst.. ":" .. item.dport .. " | " .. item.src.. ":" .. item.sport
-        if packet_hash_cmd[key] then
-            local cost = (item.tv_sec - packet_hash_sec[key]) * 1000
-            cost = cost +  (item.tv_usec - packet_hash_usec[key]) / 1000
-            if string.len(packet_hash_cmd[key]) > 20 then
-                print(string.format("%36s\t%1.20s...\t%3.3fms", key, packet_hash_cmd[key], cost))
-            else
-                print(string.format("%36s\t%-23s\t%3.3fms", key, packet_hash_cmd[key], cost))
-            end
-            packet_hash_sec[key] = nil 
-            packet_hash_usec[key] = nil 
-            packet_hash_cmd[key] = nil 
-        end
+        calculate_request_cost(key, item)
     end
 end
 
 function process_packet(item)
+    -- ignore tcp flow packet
     if item.len == 0 then
-        -- ignore tcp flow packet
         return
     end
 
+    --[[
+    --item.direct = 1 means incoming packet 
+    -- item.direct = 0 means outgoing packet 
+    --]]
     if item.direct == 1 then
-        -- item.direct = 1 means incoming packet 
         handle_incoming(item)
     else
-        -- item.direct = 0 means outgoing packet 
         handle_outgoing(item)
     end
 end
