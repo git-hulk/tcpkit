d9764437b1648794bdaa9b5c01b4c57dfe74cb77
hulk <hulk.website@gmail.com>
2018-11-22 16:24:12 +0800

    FIX: set_buffer_timeout = 0 didn't work in some os

--- src/sniffer.c
+++ src/sniffer.c
@@ -34,7 +34,7 @@ pcap_t *sniffer_packet_offline(const char *file, char *err_buf) {
 pcap_t *sniffer_packet_online(char **device, int buffer_size, char *err_buf) {
     pcap_t *pcap;
     bpf_u_int32 net = 0, mask = 0;
-    const int timeout = 0, capture_length = 1024;
+    const int timeout = 20, capture_length = 1024;
     char *new_device;
 
     if (pcap_lookupnet(*device, &net, &mask, err_buf) == -1) {
