4c90e7fdba1dbc5fc9621b51cc116165273a728b
git-hulk <hulk.website@gmail.com>
2020-08-26 20:02:18 +0800

    FIX: the print format in example.lua

--- scripts/example.lua
+++ scripts/example.lua
@@ -1,7 +1,7 @@
 function process(packet)
     if packet.size ~= 0 then -- skip the syn and ack
         local time_str = os.date('%Y-%m-%d %H:%M:%S', packet.tv_sec).."."..packet.tv_usec
-        print(string.format("%s %s:%d=>%s:%d %s %u %u %d %u %s",
+        print(string.format("%s %s:%d=>%s:%d %u %u %u %u %s",
             time_str,
             packet.sip, -- source ip
             packet.sport, -- source port
